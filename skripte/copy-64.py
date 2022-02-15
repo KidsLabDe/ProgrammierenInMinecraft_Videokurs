7b846f60c7d7e65192fdda96a3ec7de42fbbdd56
matze <matze@blade>
2022-02-15 15:05:57 +0100

    copy fixes

--- skripte/copy-64.py
+++ skripte/copy-64.py
@@ -1,16 +1,22 @@
 import os
 import time
 
-MSCS_BASE = 'mscs send StreamingKurs-0-Template'
+PLAYER = 'MatzE_'
+WORLD = 'StreamingKurs-0-Template'
 
-TP_WEITER = 'tp ~23 ~ ~'
-CLONE = 'clone 13 15 184 32 19 155 ~-1 ~-3 ~-30'
+CLONE = f'execute {PLAYER} ~ ~ ~ clone 13 15 184 32 19 155 ~-1 ~-3 ~-30'
+TP_WEITER = f'tp {PLAYER} ~23 ~ ~'
 
+mscs_base = f'mscs send {WORLD}'
+mscs_clone = f'{mscs_base} "{CLONE}"'
+mscs_tp = f'{mscs_base} "{TP_WEITER}"'
 
-mscs_clone = f'{MSCS_BASE} {CLONE}'
-mscs_tp = f'{MSCS_BASE} {TP_WEITER}'
-for _ in range(72):
-    os.system(mscs_clone)
-    time.sleep(1)
-    os.system(mscs_tp)
-    time.sleep(1)
+def main():
+    for _ in range(72):
+        os.system(mscs_clone)
+        time.sleep(1)
+        os.system(mscs_tp)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
