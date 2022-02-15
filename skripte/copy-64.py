4ffe76666ec930ece995ecc65667f184604ec485
matze <matze@kidslab.de>
2022-02-15 15:12:59 +0100

    add delay variable

--- skripte/copy-64.py
+++ skripte/copy-64.py
@@ -3,6 +3,7 @@ import time
 
 PLAYER = 'MatzE_'
 WORLD = 'StreamingKurs-0-Template'
+DELAY = 0.2
 
 CLONE = f'execute {PLAYER} ~ ~ ~ clone 13 15 184 32 19 155 ~-1 ~-3 ~-30'
 TP_WEITER = f'tp {PLAYER} ~23 ~ ~'
@@ -14,9 +15,9 @@ mscs_tp = f'{mscs_base} "{TP_WEITER}"'
 def main():
     for _ in range(72):
         os.system(mscs_clone)
-        time.sleep(1)
+        time.sleep(DELAY)
         os.system(mscs_tp)
-        time.sleep(1)
+        time.sleep(DELAY)
 
 if __name__ == '__main__':
     main()
